db482e8da466f56067ec622ad6e7ca81beb21030
Bill Moran <wmoran@potentialtech.com>
2015-02-05 16:35:02 -0500

    Comments on DSO usage in custom Adapter

--- app/src/main/java/co/tapdatapp/tapandroid/currency/BalanceListAdapter.java
+++ app/src/main/java/co/tapdatapp/tapandroid/currency/BalanceListAdapter.java
@@ -39,12 +39,14 @@ public class BalanceListAdapter implements ListAdapter {
 
     @Override
     public void registerDataSetObserver(DataSetObserver dataSetObserver) {
-        //
+        // The dataset is static. When it changes a new adapter is
+        // created, so DSOs don't need to be tracked.
     }
 
     @Override
     public void unregisterDataSetObserver(DataSetObserver dataSetObserver) {
-        //
+        // The dataset is static. When it changes a new adapter is
+        // created, so DSOs don't need to be tracked.
     }
 
     @Override
