842484bb0c0725d15920068172aaca4271eecbd7
Bill Moran <wmoran@potentialtech.com>
2015-02-09 15:10:59 -0500

    Switch to using Android 5 themes, which breaks some things and will require some rework of the navigation to get it back to where it was.

--- app/src/main/java/co/tapdatapp/tapandroid/MainActivity.java
+++ app/src/main/java/co/tapdatapp/tapandroid/MainActivity.java
@@ -31,6 +31,7 @@ import android.provider.MediaStore;
 import android.support.v13.app.FragmentPagerAdapter;
 import android.os.Bundle;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -52,8 +53,7 @@ import de.hdodenhof.circleimageview.CircleImageView;
 
 public class MainActivity
 extends Activity
-implements AccountFragment.OnFragmentInteractionListener,
-           ActionBar.TabListener {
+implements AccountFragment.OnFragmentInteractionListener {
 
     SectionsPagerAdapter mSectionsPagerAdapter;
     ViewPager mViewPager;
@@ -140,34 +140,10 @@ implements AccountFragment.OnFragmentInteractionListener,
 
             //set up action bar and nav tabs
             mSectionsPagerAdapter = new SectionsPagerAdapter(getFragmentManager());
-            // Set up the action bar.
-            final ActionBar actionBar = getActionBar();
-            // Specify that the Home/Up button should not be enabled, since there is no hierarchical
-            // parent.
-            actionBar.setHomeButtonEnabled(false);
-            // Specify that we will be displaying tabs in the action bar.
-            actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+
             // Set up the ViewPager with the sections adapter.
             mViewPager = (ViewPager) findViewById(R.id.pager);
             mViewPager.setAdapter(mSectionsPagerAdapter);
-            mViewPager.setOnPageChangeListener(new ViewPager.SimpleOnPageChangeListener() {
-                @Override
-                public void onPageSelected(int position) {
-                    // When swiping between different app sections, select the corresponding tab.
-                    // We can also use ActionBar.Tab#select() to do this if we have a reference to the
-                    // Tab.
-                    actionBar.setSelectedNavigationItem(position);
-                }
-            });
-            for (int i = 0; i < mSectionsPagerAdapter.getCount(); i++) {
-                // Create a tab with text corresponding to the page title defined by the adapter.
-                // Also specify this Activity object, which implements the TabListener interface, as the
-                // listener for when this tab is selected.
-                actionBar.addTab(
-                        actionBar.newTab()
-                                .setText(mSectionsPagerAdapter.getPageTitle(i))
-                                .setTabListener(this));
-            }
 
             //sets home page to tap
 
@@ -651,17 +627,6 @@ implements AccountFragment.OnFragmentInteractionListener,
         return mTapUser;
     }
 
-    @Override
-    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
-    @Override
-    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-        // When the given tab is selected, switch to the corresponding page in the ViewPager.
-        mViewPager.setCurrentItem(tab.getPosition());
-    }
-    @Override
-    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
-    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
