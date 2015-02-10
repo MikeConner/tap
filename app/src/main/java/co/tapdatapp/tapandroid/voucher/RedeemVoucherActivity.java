31dca1c4c7ffd02f8ae142d52d058acd20b0eb72
Bill Moran <wmoran@potentialtech.com>
2015-02-10 13:14:08 -0500

    Initial shell of new voucher redeem screen. Lots yet to do.

---
+++ app/src/main/java/co/tapdatapp/tapandroid/voucher/RedeemVoucherActivity.java
@@ -0,0 +1,29 @@
+/**
+ * Redeem a voucher to charge up an currency
+ */
+
+package co.tapdatapp.tapandroid.voucher;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+
+import co.tapdatapp.tapandroid.R;
+
+public class RedeemVoucherActivity extends Activity {
+
+    @Override
+    public void onCreate(Bundle b) {
+        super.onCreate(b);
+        setContentView(R.layout.redeem_voucher_activity);
+    }
+
+    /**
+     * ??
+     *
+     * @param v The button that was clicked
+     */
+    public void clickLoadVoucher(View v) {
+
+    }
+}
