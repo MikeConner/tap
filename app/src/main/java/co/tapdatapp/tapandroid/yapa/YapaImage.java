82053c112c7b0a1e6d92837ff82e80e58e79e485
vam25 <vinceamarino@gmail.com>
2015-02-13 17:48:37 -0500

    Added more to the new yapa activity

--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaImage.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaImage.java
@@ -1,11 +1,25 @@
 package co.tapdatapp.tapandroid.yapa;
 
 import android.app.Activity;
+import android.os.Bundle;
+
+import co.tapdatapp.tapandroid.R;
 
 /**
  * Created by Vince on 2/13/2015.
  */
 public class YapaImage extends Activity {
 
-    
+    public void onCreate(Bundle state) {
+        super.onCreate(state);
+        setContentView(R.layout.activity_yapa_image);
+    }
+
+    public void onResume() {
+        super.onResume();
+    }
+
+    public void onPause(){
+        super.onPause();
+    }
 }
