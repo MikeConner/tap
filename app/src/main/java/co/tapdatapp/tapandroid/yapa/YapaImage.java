185532342ae69d9007390e92d6e13ddf63802662
vam25 <vinceamarino@gmail.com>
2015-02-14 15:47:15 -0500

    More changes to the Yapa Layout

--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaImage.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaImage.java
@@ -1,7 +1,16 @@
 package co.tapdatapp.tapandroid.yapa;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Display;
+import android.view.View;
+import android.widget.ImageView;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.widget.LinearLayout;
 
 import co.tapdatapp.tapandroid.R;
 
@@ -21,5 +30,12 @@ public class YapaImage extends Activity {
 
     public void onPause(){
         super.onPause();
+    }
+
+    /**
+     * Doesn't work yet
+     */
+    public void goFullscreen(){
+
     }
 }
