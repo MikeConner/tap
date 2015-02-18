fd9725cd09d74b7b923d1c575eccde2bb23ea219
vam25 <vinceamarino@gmail.com>
2015-02-17 23:47:14 -0500

    Yapa page for URLs also shows real info.

--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaUrl.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaUrl.java
@@ -8,8 +8,10 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.content.ContentUris;
+import android.widget.TextView;
 
 import co.tapdatapp.tapandroid.R;
+import co.tapdatapp.tapandroid.localdata.Transaction;
 
 public class YapaUrl extends Activity{
 
@@ -17,7 +19,15 @@ public class YapaUrl extends Activity{
         super.onCreate(state);
         setContentView(R.layout.activity_yapa_url);
         final ImageView imageView = (ImageView) findViewById(R.id.yapaUrl);
+        final TextView urlSender = (TextView) findViewById(R.id.url_sender);
+        final TextView urlDescription = (TextView) findViewById(R.id.url_description);
+        final TextView urlDate = (TextView) findViewById(R.id.url_date);
 
+        final Transaction transaction = new Transaction();
+        ((TextView)urlSender.findViewById(R.id.url_sender)).setText(transaction.getNickname());
+        ((TextView)urlDescription.findViewById(R.id.url_description)).setText(transaction.getDescription());
+        ((TextView)urlDate.findViewById(R.id.url_date)).setText(transaction.getTimestamp().toString()
+                + "  " + Integer.toString(transaction.getAmount()));
         /**
          * This opens up a webpage with the desired url
          */
@@ -27,7 +37,7 @@ public class YapaUrl extends Activity{
                 Intent goToURL = new Intent();
                 goToURL.setAction(Intent.ACTION_VIEW);
                 goToURL.addCategory(Intent.CATEGORY_BROWSABLE);
-                goToURL.setData(Uri.parse("http://google.com"));
+                goToURL.setData(Uri.parse(transaction.getYapa_url()));
                 startActivity(goToURL);
             }
         });
