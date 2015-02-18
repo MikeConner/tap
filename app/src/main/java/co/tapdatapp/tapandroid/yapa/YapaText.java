df9a252be088bb46785ca6f62f4d17253a2b99c6
vam25 <vinceamarino@gmail.com>
2015-02-17 23:38:25 -0500

    Yapa page for text also shows actual info

--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaText.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaText.java
@@ -2,14 +2,26 @@ package co.tapdatapp.tapandroid.yapa;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import co.tapdatapp.tapandroid.R;
+import co.tapdatapp.tapandroid.localdata.Transaction;
 
 public class YapaText extends Activity {
 
     public void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.activity_yapa_text);
+
+        final TextView textSender = (TextView) findViewById(R.id.text_sender);
+        final TextView textDescription = (TextView) findViewById(R.id.text_description);
+        final TextView textDate = (TextView) findViewById(R.id.text_date);
+
+        Transaction transaction = new Transaction();
+        ((TextView)textSender.findViewById(R.id.text_sender)).setText(transaction.getNickname());
+        ((TextView)textDescription.findViewById(R.id.text_description)).setText(transaction.getDescription());
+        ((TextView)textDate.findViewById(R.id.text_date)).setText(transaction.getTimestamp().toString()
+                + "  " + Integer.toString(transaction.getAmount()));
     }
 
     public void onResume() {
