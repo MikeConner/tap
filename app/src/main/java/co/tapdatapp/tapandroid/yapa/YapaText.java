29db3c1dab45685142212349331d3077cdaaba79
vam25 <vinceamarino@gmail.com>
2015-03-09 02:17:36 -0400

    Fixed the URL and Text Yapa pages.

--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaText.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaText.java
@@ -1,20 +1,27 @@
 package co.tapdatapp.tapandroid.yapa;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
+import co.tapdatapp.tapandroid.MainActivity;
 import co.tapdatapp.tapandroid.R;
 import co.tapdatapp.tapandroid.localdata.Transaction;
 
 public class YapaText extends Activity {
 
+    private boolean forceReturnToArmScreen = false;
+
     public void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.activity_yapa_text);
-        final int transactionId = getIntent().getExtras().getInt(YapaDisplay.TRANSACTION_ID);
+        Bundle extras = getIntent().getExtras();
+        final String transactionId = extras.getString(YapaDisplay.TRANSACTION_ID);
         final Transaction transaction = new Transaction();
-        transaction.moveToByOrder(transactionId);
+        transaction.moveToSlug(transactionId);
 
         final TextView textSender = (TextView) findViewById(R.id.text_sender);
         final TextView textDescription = (TextView) findViewById(R.id.text_description);
@@ -23,6 +30,37 @@ public class YapaText extends Activity {
         textSender.setText(transaction.getNickname());
         textDescription.setText(transaction.getDescription());
         textDate.setText(transaction.getTimestamp().toString() + "  " + Integer.toString(transaction.getAmount()));
+
+        int showTime = extras.getInt(YapaDisplay.DELAY_TIME, -1);
+        if (showTime != -1) {
+            /**
+             * This makes the view disappear after the specified wait
+             */
+            forceReturnToArmScreen = true;
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    onBackPressed();
+                }
+            };
+            YapaDisplay.delayWorker.schedule(task, showTime, TimeUnit.SECONDS);
+        }
+    }
+
+    /**
+     * Makes pressing back go to the arm screen.
+     *
+     * if the concern that this is bad turns out to be true, this also needs to be changed.
+     */
+    @Override
+    public void onBackPressed() {
+        if (forceReturnToArmScreen) {
+            Intent startMain = new Intent(this, MainActivity.class);
+            startActivity(startMain);
+        }
+        else {
+            finish();
+        }
     }
 }
 
--- app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaUrl.java
+++ app/src/main/java/co/tapdatapp/tapandroid/yapa/YapaUrl.java
@@ -2,30 +2,31 @@ package co.tapdatapp.tapandroid.yapa;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.graphics.Bitmap;
 import android.net.Uri;
-import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import java.util.concurrent.TimeUnit;
+
+import co.tapdatapp.tapandroid.MainActivity;
 import co.tapdatapp.tapandroid.R;
-import co.tapdatapp.tapandroid.TapApplication;
-import co.tapdatapp.tapandroid.helpers.TapBitmap;
 import co.tapdatapp.tapandroid.localdata.Transaction;
 
 public class YapaUrl extends Activity{
 
-    public final static String TRANSACTION_ID = "TxId";
+    private boolean forceReturnToArmScreen = false;
 
     public void onCreate(Bundle state) {
         super.onCreate(state);
         setContentView(R.layout.activity_yapa_url);
 
-        final int transactionId = getIntent().getExtras().getInt(TRANSACTION_ID);
+        Bundle extras = getIntent().getExtras();
+        final String transactionId = extras.getString(YapaDisplay.TRANSACTION_ID);
         final Transaction transaction = new Transaction();
-        transaction.moveToByOrder(transactionId);
+        transaction.moveToSlug(transactionId);
+
         final ImageView imageView = (ImageView) findViewById(R.id.yapaUrl);
         final TextView urlSender = (TextView) findViewById(R.id.url_sender);
         final TextView urlDescription = (TextView) findViewById(R.id.url_description);
@@ -48,44 +49,60 @@ public class YapaUrl extends Activity{
                 startActivity(goToURL);
             }
         });
+
+        int showTime = extras.getInt(YapaDisplay.DELAY_TIME, -1);
+        if (showTime != -1) {
+            /**
+             * This makes the view disappear after the specified wait
+             */
+            forceReturnToArmScreen = true;
+            Runnable task = new Runnable() {
+                @Override
+                public void run() {
+                    onBackPressed();
+                }
+            };
+            YapaDisplay.delayWorker.schedule(task, showTime, TimeUnit.SECONDS);
+        }
     }
+
     /**
-     * Load the image onto the view in the background. This has to be a background task because
-     * the image may not be in the local cache, and thus a network fetch would be required.
-     */
-    private class ImageFetchTask extends AsyncTask<Object, Void, Void> {
+     * Ideally this would be a logo of the webpage or something that gets passed with the transaction.
+     * I currently have it set to the Tap logo. It could also be a preview for the webpage, but I suspect that wouldn't
+     * be handled the same way as fetching an image. Need to ask Katherine about it.
+     *
+    @Override
+    public void onImageRetrieved(Bitmap image) {
+        imageView.setImageBitmap(image);
+    }
 
-        Bitmap imageBitmap = null;
-        ImageView imageView = null;
-
-        /**
-         * @param params ImageView to set and Transaction to set from
-         * @return Void
-         */
-        @Override
-        protected Void doInBackground(Object... params) {
-            if (params.length != 2) {
-                throw new AssertionError("Requires ImageView and transaction");
-            }
-            imageView = (ImageView)params[0];
-            Transaction transaction = (Transaction)params[1];
-            try {
-                imageBitmap = TapBitmap.fetchFromCacheOrWeb(transaction.getThumb_url());
-            }
-            catch (Exception e) {
-                TapApplication.unknownFailure(e);
-            }
-            return null;
+    @Override
+    public void onImageRetrievalError(Throwable t) {
+        try {
+            throw t;
         }
-
-        protected void onPostExecute(Void x) {
-            //noinspection StatementWithEmptyBody
-            if (imageBitmap != null) {
-                imageView.setImageBitmap(imageBitmap);
-            }
-            else {
-                // @TODO provide some sort of message to the user that the image can't be displayed
-            }
+        catch (UserFriendlyError ufe) {
+            TapApplication.errorToUser(ufe);
+        }
+        catch (Throwable t1) {
+            TapApplication.unknownFailure(t1);
         }
     }
+    **/
+
+    /**
+     * This makes it go to the arm screen when the user hits back.
+     */
+    // May or may not be a problem with this approach in the long-term
+    @Override
+    public void onBackPressed() {
+        if (forceReturnToArmScreen) {
+            Intent startMain = new Intent(this, MainActivity.class);
+            startActivity(startMain);
+        }
+        else {
+            finish();
+        }
+    }
+
 }
