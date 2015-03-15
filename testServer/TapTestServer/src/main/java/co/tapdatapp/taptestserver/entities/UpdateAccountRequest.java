9350b24a30c485a31df7b65b676be0891ca8b2fd
Bill Moran <wmoran@potentialtech.com>
2015-03-15 14:51:47 -0400

    Rudimentary simulation of Amazon's storage cluster.

--- testServer/TapTestServer/src/main/java/co/tapdatapp/taptestserver/entities/UpdateAccountRequest.java
+++ testServer/TapTestServer/src/main/java/co/tapdatapp/taptestserver/entities/UpdateAccountRequest.java
@@ -10,23 +10,46 @@ public class UpdateAccountRequest {
   public UserObject user;
 
   public class UserObject {
-    public String name;
-    public String email;
-    public String mobile_profile_thumb_url;
+    public String name = null;
+    public String email = null;
+    public String mobile_profile_thumb_url = null;
   }
  
   /**
    * Having difficulty convincing Jersey to automatically build this object,
    * so just brute-force it.
    * 
+   * If any of the items are not found in the request, just leave them null
+   * 
    * @param json the raw JSON
    */
   public UpdateAccountRequest(String json) throws JSONException {
     JSONObject jo = new JSONObject(json).getJSONObject("user");
     user = new UserObject();
-    user.name = jo.getString("name");
-    user.email = jo.getString("email");
-    user.mobile_profile_thumb_url = jo.getString("mobile_profile_thumb_url");
+    try {
+      user.name = jo.getString("name");
+    }
+    catch (JSONException je) {
+      if (!je.getMessage().contains("not found")) {
+        throw je;
+      }
+    }
+    try {
+      user.email = jo.getString("email");
+    }
+    catch (JSONException je) {
+      if (!je.getMessage().contains("not found")) {
+        throw je;
+      }
+    }
+    try {
+      user.mobile_profile_thumb_url = jo.getString("mobile_profile_thumb_url");
+    }
+    catch (JSONException je) {
+      if (!je.getMessage().contains("not found")) {
+        throw je;
+      }
+    }
   }
   
 }
